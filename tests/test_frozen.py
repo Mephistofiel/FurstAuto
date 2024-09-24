2ef4ab93746882fef37b39f07aee7c7f38f9a635
Mephistofiel <bvgthbzpkf@gmail.com>
2024-09-24 18:35:17 +0400

    first action v.0.1.4

--- tests/test_frozen.py
+++ tests/test_frozen.py
@@ -13,27 +13,27 @@ from pages.navigation import NavigationPage
 @allure.title("Тест проверки текста")
 @pytest.mark.text
 def test_text(browser):
-    page = TextsPage(browser, json_file='data.json', url='file:///C:/HTML/Mostbet/(MB)%20frozen_account_RU.html')
+    page = TextsPage(browser, json_file='data.json', url='https://github.com/Mephistofiel/HTML/blob/master/Mostbet/(MB)%20frozen_account_RU.html')
     page.open_page()
     page.check_texts_by_id('1')
 
 @allure.title("Тест проверки ссылок")
 @pytest.mark.links
 def test_links(browser):
-    page = LinksPage(browser, json_file='data.json', url='file:///C:/HTML/Mostbet/(MB)%20frozen_account_RU.html')
+    page = LinksPage(browser, json_file='data.json', url='https://github.com/Mephistofiel/HTML/blob/master/Mostbet/(MB)%20frozen_account_RU.html')
     page.open_page()
     page.check_links_by_id('1')
 
 @allure.title("Тест проверки изображений")
 @pytest.mark.image
 def test_images(browser):
-    page = ImagesPage(browser, json_file='data.json', url='file:///C:/HTML/Mostbet/(MB)%20frozen_account_RU.html')
+    page = ImagesPage(browser, json_file='data.json', url='https://github.com/Mephistofiel/HTML/blob/master/Mostbet/(MB)%20frozen_account_RU.html')
     page.open_page()
     page.check_images_by_id('1')
 
 @allure.title("Тест проверки перехода на страницы")
 @pytest.mark.navigate
 def test_navigate(browser):
-    page = NavigationPage(browser, json_file='data.json', url='file:///C:/HTML/Mostbet/(MB)%20frozen_account_RU.html')
+    page = NavigationPage(browser, json_file='data.json', url='https://github.com/Mephistofiel/HTML/blob/master/Mostbet/(MB)%20frozen_account_RU.html')
     page.open_page()
     page.navigate_links_by_id('1')
